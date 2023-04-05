bb8d460b682d689d4992d6904ee1ec05dd437a31
Macker85 <pmacker1985@gmail.com>
2023-04-05 17:41:32 +0000

    spelling errors fixed in models.py

--- blog/models.py
+++ blog/models.py
@@ -1,18 +1,19 @@
 from django.db import models
-from django.contrib.auth.models import user
+from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
 
-class Post(model.Model):
+class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts")
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
-    excerpt = models.TextField(auto_now_add=True)
+    excerpt = models.TextField(blank=True)
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
 
@@ -28,10 +29,10 @@ class Post(model.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    name = models.Charfield(max_length=80)
+    name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
-    created_on = modles.DateTimeField(auto_now_add=true)
+    created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
 
     class Meta:
